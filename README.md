9c6ed1ce08d3d77d4c4e88532da80379f1567bda
Mayank Soni <mayanksoni@nicobar.com>
2020-03-16 14:49:33 +0530

    First commit, structure setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Almabase-assignment
+Assignment for Almabase
---
+++ src/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './index.css';
+
+class Home extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return (
+            <React.Fragment>
+                <h1>Homepage</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Home;
